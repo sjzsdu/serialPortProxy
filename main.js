a8e076ad254a011563c9205edcd1224ebe32be2e
sjzsdu <122828837@qq.com>
2019-09-12 16:47:50 +0800

    连接rfid串口OK

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
 const path = require('path')
+const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -8,19 +13,28 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+  mainWindow = new BrowserWindow(
+    {
+      width: 600, 
+      height: 400, 
+      icon: './public/img/icon.png',
+      autoHideMenuBar:true,
+      webPreferences: {
+        nodeIntegration:true
+      }
     }
-  })
+  )
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -38,15 +52,19 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
+  // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
+  // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
+  if (mainWindow === null) {
+    createWindow()
+  }
 })
 
 // In this file you can include the rest of your app's specific main process
---
+++ public/js/util.js
@@ -0,0 +1,65 @@
+const util = {
+    strToHexCharCode(str) {
+    　　if(str === "")
+    　　　　return "";
+    　　var hexCharCode = [];
+    　　hexCharCode.push("0x"); 
+    　　for(var i = 0; i < str.length; i++) {
+    　　　　hexCharCode.push((str.charCodeAt(i)).toString(16));
+    　　}
+    　　return hexCharCode.join("");
+    },
+
+    hexCharCodeToStr(hexCharCodeStr) {
+    　　var trimedStr = hexCharCodeStr.trim();
+    　　var rawStr = 
+    　　trimedStr.substr(0,2).toLowerCase() === "0x"
+    　　? 
+    　　trimedStr.substr(2) 
+    　　: 
+    　　trimedStr;
+    　　var len = rawStr.length;
+    　　if(len % 2 !== 0) {
+    　　　　alert("Illegal Format ASCII Code!");
+    　　　　return "";
+    　　}
+    　　var curCharCode;
+    　　var resultStr = [];
+    　　for(var i = 0; i < len;i = i + 2) {
+    　　　　curCharCode = parseInt(rawStr.substr(i, 2), 16); // ASCII Code Value
+    　　　　resultStr.push(String.fromCharCode(curCharCode));
+    　　}
+    　　return resultStr.join("");
+    },
+
+    intToAscii(intArr){
+        const sarr = []
+        intArr.forEach(item => {
+            sarr.push(String.fromCharCode(item))
+        })
+        return sarr.join('');
+    },
+
+    intToHex(intArr){
+        const sarr = []
+        const len = intArr.length
+        const n = Math.ceil(len / 24);
+        console.log('初始化返回的数据', intArr,n,len)
+        for(let i =0; i<n; i++){
+            let one = []
+            for(let j=8; j<20;j++){
+                let index = i*24+j;
+                if(typeof intArr[index] !== "undefined"){
+                    let str = intArr[index].toString(16)
+                    if(str.length == 1)
+                        str = '0' + str
+                    one.push(str.toUpperCase())
+                } 
+            }
+            sarr.push(one.join(''))
+        }
+        return sarr;
+    }
+}
+
+module.exports = util;
